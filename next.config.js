1551677cecd9215222ecd4e4b38b71035a43ff8a
Nontakorn Nonsurat <nontakorn2600@gmail.com>
2025-11-15 22:36:28 +0700

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,34 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compress: true,
-  poweredByHeader: false,
-  
-  headers: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-          },
-        ],
-      },
-    ];
-  },
-
-  rewrites: async () => {
-    return {
-      beforeFiles: [
-        {
-          source: '/data/:path*',
-          destination: `${process.env.NEXT_PUBLIC_FANTROVE_DATA_URL}/:path*`,
-        },
-      ],
-    };
-  },
+  swcMinify: true
 };
 
 module.exports = nextConfig;
